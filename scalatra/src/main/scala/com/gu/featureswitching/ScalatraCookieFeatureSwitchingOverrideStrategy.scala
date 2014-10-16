1b285e45b59ca65decb6f3394b6c8a7dfa2146c7
Paul Roberts <paul.roberts@guardian.co.uk>
2014-10-16 15:48:21 +0100

    remove leftover import

--- scalatra/src/main/scala/com/gu/featureswitching/ScalatraCookieFeatureSwitchingOverrideStrategy.scala
+++ scalatra/src/main/scala/com/gu/featureswitching/ScalatraCookieFeatureSwitchingOverrideStrategy.scala
@@ -1,7 +1,6 @@
 package com.gu.featureswitching
 
 import org.scalatra._
-import java.net.{URLDecoder, URLEncoder}
 
 trait ScalatraCookieFeatureSwitchingOverrideStrategy extends
     CookieFeatureSwitchingOverrideStrategy with ScalatraKernel with CookieSupport {
