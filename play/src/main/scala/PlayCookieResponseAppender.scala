6ac3e160a07491a5c8f3c0abe3b7f04663cfafe9
Robert Kenny <kenoir@gmail.com>
2014-11-13 13:32:53 +0000

    remove cookie override strategy

--- play/src/main/scala/PlayCookieResponseAppender.scala
+++
@@ -1,9 +0,0 @@
-package com.gu.featureswitching.play
-
-import play.api.mvc.{Cookies, Cookie, Result}
-
-trait PlayResultCookieAdder extends Cookies {
-  def appendCookies(result: Result, cookies: Option[Cookie]) {
-    cookies.foreach {result.withCookies(_)}
-  }
-}
